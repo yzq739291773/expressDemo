f1deed214f3c154a44ae2b03aaccf62758fe5ea0
LAPTOP-QE08G43O\admin <xiongwz@ljfkj.com>
2017-11-22 09:38:43 +0800

    临时

--- src/controllers/accountControllers.js
+++ src/controllers/accountControllers.js
@@ -1,9 +1,15 @@
 // 导包
-// 。。。
+const express = require('express')
 
 // 导出控制器的方法即处理具体逻辑的方法
 exports.getLogin = (req, res) => {
     // 具体逻辑代码
+    res.setHearder("content-type", "text/html;charset=urf-8")
+    res.json({
+        status: '200',
+        msg: 'sucess',
+        result: 'hello world'
+    })
 }
 exports.getVcode = (req, res) => {
 
